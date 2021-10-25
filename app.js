b3356a8152ce50c255510c52fe55f03b13378cb6
fabrivc <90659637+fabrivc@users.noreply.github.com>
2021-10-25 14:30:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-const express = require ( "express" )
-const path = require ( "path" )
+const express = require ("express")
+const path = require("path")
 const app = express()
 
-const port = process.env.PORT||3000;
-app.use("/static",express.static("public"));
+const port = process.env.PORT || 3030;
 
-app.listen (port,() => {
-    console.log ("App Corriendo")
-});
+const publicFolderPath = path.resolve(__dirname, "./public")
+app.use(express.static(publicFolderPath));
 
-app.get ("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"))
-});
-app.get ("/misCompras.html", (req,res) => {
+app.listen (port, ()=> {
+    console.log("Servidor funcionando")
+})
+
+app.get ("/", (req,res) => {
+    res.sendFile (path.join (__dirname, "/views/home.html"))
+})
+
+
+app.get ("/home", (req,res) => {
+    res.sendFile (path.join (__dirname, "/views/home.html"))
+})
+
+app.get ("/mis-compras", (req,res) => {
     res.sendFile (path.join (__dirname, "/views/misCompras.html"))
-});
+})
 
-app.get ("/ingresa.html", (req,res) => {
+app.get ("/ingresa", (req,res) => {
     res.sendFile (path.join (__dirname, "/views/ingresa.html"))
-});
+})
 
-app.get ("/creaTuCuenta.html", (req,res) => {
+app.get ("/creaTuCuenta", (req,res) => {
     res.sendFile (path.join (__dirname, "/views/creaTuCuenta.html"))
-});
+})
+
+app.get ("/ayuda", (req,res) => {
+res.sendFile (path.join (__dirname, "/views/ayuda.html"))
+})
+
+app.get ("/vender", (req,res) => {
+res.sendFile (path.join (__dirname, "/views/vender.html"))
+})
+
+app.get ("/tiendasOficiales", (req,res) => {
+res.sendFile (path.join (__dirname, "/views/tiendasOficiales.html"))
+})
+
+app.get ("/ofertas", (req,res) => {
+res.sendFile (path.join (__dirname, "/views/ofertas.html"))
+})
